c69c38c561ac5fc5e8b46a2e9f355afa1d797a54
LAROUCIGHEZALA <100726445+LAROUCIGHEZALA@users.noreply.github.com>
2022-06-18 17:46:49 +0200

    Create Classr.cs

---
+++ Classr.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp1
+{
+    class Classr
+    {
+       
+            public Classr(String number, string name)
+            {
+                Number = number;
+            Name = name;
+            }
+
+       
+
+        public String Number { get; set; }
+            public String Name{ get; set; }
+     
+    
+    }
+}
